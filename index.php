bb2f3ab08a253565112d951211426a62d93a1c23
Veronica Barbieri <veronica.barbieri.p@gmail.com>
2019-10-25 11:11:12 +0200

    Struttura base model view controller per la pagina index, richiamo di header e footer

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+  $pagein = array(
+    'Namepage' => 'Homepage' ,
+  );
+
+  require_once 'Views/header.php';
+  require_once 'Models/index.php';
+  require_once 'Views/index.php';
+  require_once 'Views/footer.php';
+  
+ ?>

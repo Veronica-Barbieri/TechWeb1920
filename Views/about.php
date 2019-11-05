b4b3c38344f3307db13daff343896bdd9d1c49ef
Yukamo <54179163+Yukamo@users.noreply.github.com>
2019-11-05 18:43:10 +0100

    Creazione file about.php per la parte di view ed di controller
    La parte di gestione e ricezione dei dati verra gestita da un file chiamato email.php

---
+++ Views/about.php
@@ -0,0 +1,20 @@
+<div class="box" id="presentation">
+	<h1> Benvenuto giovane avventuriero!</h1>
+	<p> Vi diamo il benvenuto nella nostra pagina di informazioni riguardanti il nostro sito.
+	Il nostro sito di <span xml:lang="en"> Magic: The Gathering </span> &eacute; stato creato per aiutare giovani ed esperti a creare i propri deck personalizzati in modo facile e veloce.
+	Se avete domande particolari che volete chiederci basta contattarci nella<span xml:lang="en"> form</span> sottostante;
+	Buon Divertimento!
+	</p>
+</div>
+
+<div class="box" id="form">
+	<h2><span xml:lang="en">Form </span>di contatto</h2>
+	<form action="email.php" method="post">
+	Nome: <input type="text" name="nome">
+	Cognome: <input type="text" name="cognome"><br>
+	E-mail: <input type="text" name="email"><br>
+	Oggetto: <input type="text" name="oggetto"><br>
+	<input type="text" name="testo"><br>
+	<input type="submit">
+	</form>
+</div>
---
+++ about.php
@@ -0,0 +1,9 @@
+<?php
+  $pagein = array(
+    'Namepage' => 'About Us'
+  );
+  require_once ("Views/header.php");
+  require_once ("Models/about.php");
+  require_once ("Views/about.php");
+  require_once ("Views/footer.php");
+ ?>

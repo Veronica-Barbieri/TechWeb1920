b7ce12088d0255824795ad6ac085834d8779eef7
Veronica Barbieri <veronica.barbieri.p@gmail.com>
2019-11-05 13:11:11 +0100

    Creata la pagina di login
    Struttura base pagina di login
    close #5

---
+++ Views/login.php
@@ -0,0 +1,26 @@
+<form class="login_frm" action="usr_page.php" method="post">
+  <fieldset>
+    <legend>Accedi al tuo account</legend>
+    <input type="text" name="username" placeholder="Il tuo Username">
+    <input type="password" name="pwd" placeholder="La tua Password">
+    <input type="submit" name="login_input" value="Login">
+  </fieldset>
+</form>
+<!--
+<style media="screen">
+  fieldset{
+    margin: auto;
+    width: 50%;
+    text-align: center;
+  }
+  fieldset > input {
+    display: inline-block;
+    margin: 0.5em;
+  }
+  fieldset > legend {
+    padding: 0.3em;
+    margin-left: 0.5em;
+  }
+
+</style>
+-->
---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+$pagein = array(
+  'Namepage' => 'Login'
+);
+
+//error_reporting (E_ALL);
+require_once ("Views/header.php");
+require_once ("Views/login.php");
+require_once ("Views/footer.php");
+ ?>

41683d0973009defbdb8cc55361c8eee42b287d1
MCarbona <45630302+MCarbona@users.noreply.github.com>
2019-11-06 15:07:05 +0100

    Aggiunto file single_card

---
+++ Views/single_card.php
@@ -0,0 +1,18 @@
+<body id="homepage">
+<div id="caratteristiche">
+  <h1>Caratteristiche carta</h1>
+  <ul>
+    <li>Nome carta</li>
+    <li>Costo in ?</li>
+    <li>Tipo</li>
+    <li>Descrizione</li>
+    <li>Flavour text</li>
+    <li>Espansione</li>
+    <li>Rarità</li>
+    <li>Numero carta</li>
+    <li>Artista immagine</li>
+  </ul>
+  <img xml:lang="en" id="" src="images/.jpeg" >
+</div>
+</body>
+</html>
---
+++ single_card.php
@@ -0,0 +1,4 @@
+<?php require_once("Views/header.php");
+require_onece("Views/single_card.php");
+require_once("Views/footer.php");
+?>

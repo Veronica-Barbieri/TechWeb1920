aa11daa599f762c35ffd122f9eb3fa857f510385
Veronica Barbieri <veronica.barbieri.p@gmail.com>
2019-11-06 16:36:49 +0100

    Creata la pagina delle regole

---
+++ Views/regole.php
@@ -0,0 +1,114 @@
+<h1>Glossario</h1>
+<a href="https://magicthegathering.fandom.com/it/wiki/Glossario">
+  Per leggere un glossario di tutti i termini utili, CLICCA QUI!
+</a>
+<h1>Le basi del gioco</h1>
+<p>
+  Per iniziare una partita, rimescola il tuo mazzo, che viene anche chiamato
+  grimorio. Pesca una mano di sette carte e conta le terre. Puoi controllare la
+  riga di testo subito sotto l’illustrazione di una carta per capire di che tipo
+  è.
+</p>
+<p>
+  Tu e il tuo avversario cominciate la partita con 20 punti vita ciascuno,
+  che dovrete entrambi annotare in qualche modo (ad esempio, usando un dado
+  oppure carta e penna). Riduci a 0 i punti vita del tuo avversario e vincerai
+  la partita!
+ </p>
+<h1>Tipi di carte</h1>
+<ul>
+  <li>
+    <h2>Terre</h2>
+    <p>
+      Le carte terra costituiscono la base di qualsiasi mazzo efficace di Magic,
+      perché producono mana, l’energia magica necessaria per lanciare tutte le
+      altre carte del mazzo. Puoi giocare una terra durante la fase principale
+      di ognuno dei tuoi turni e queste terre rimangono di fronte a te sul
+      campo di battaglia. Le tue terre vengono STAPpate (raddrizzate) all’inizio
+      di ogni tuo turno, pronte per essere usate di nuovo.
+     </p>
+     <p>
+       Esistono cinque tipi di terre base, ognuno corrispondente a uno dei
+       cinque colori
+     </p>
+     <ul>
+       <li> <span>Pianura</span> <img src="Resources/White_Mana.png" alt="Bianco"> </li>
+       <li> <span>Isola</span> <img src="Resources/Blue_Mana.png" alt="Bianco"> </li>
+       <li> <span>Palude</span> <img src="Resources/Black_Mana.png" alt="Bianco"> </li>
+       <li> <span>Montagna</span> <img src="Resources/Red_Mana.png" alt="Bianco"> </li>
+       <li> <span>Foresta</span> <img src="Resources/Green_Mana.png" alt="Bianco"> </li>
+     </ul>
+  </li>
+  <li>
+    <h2>Magie</h2>
+    <p>
+      Qualsiasi carta non terra può essere lanciata come magia. Alcuni tipi di
+      magia vengono messi sul campo di battaglia e diventano “permanenti”,
+      altri producono il loro effetto e poi vengono messi nel tuo cimitero (la
+      pila degli scarti).
+    </p>
+  </li>
+  <li>
+    <h2>Creature</h2>
+    <p>
+      Le creature sono permanenti che rappresentano i seguaci al tuo comando.
+      Durante il combattimento, possono attaccare gli avversari e difenderti
+      dagli assalti nemici. Puoi lanciare le magie creatura durante la tua fase
+      principale.
+    </p>
+  </li>
+  <li>
+    <h2>Artefatti e incantesimi</h2>
+    <p>
+      Gli artefatti e gli incantesimi sono permanenti che rappresentano oggetti
+      magici e manifestazioni magiche stabili. Molti artefatti sono incolori,
+      quindi non richiedono mana di un colore specifico per essere lanciati.
+      Alcuni artefatti sono anche creature. Puoi lanciare le magie artefatto e
+      incantesimo durante la tua fase principale.
+    </p>
+  </li>
+  <li>
+    <h2>Stregonerie e istantanei</h2>
+    <p>
+      Le magie stregoneria e istantaneo rappresentano potenti incantamenti che
+      possono influenzare la partita in molti modi diversi. Dopo che il loro
+      effetto è avvenuto, queste magie vengono messe nel tuo cimitero. Puoi
+      lanciare una stregoneria solo durante la tua fase principale, quando
+      potresti lanciare creature e altre magie permanente. Gli istantanei,
+      invece, possono essere lanciati nel turno di qualsiasi giocatore,
+      praticamente in ogni momento. . . addirittura in risposta a un’altra magia!
+    </p>
+  </li>
+  <li>
+    <h2>Planeswalker</h2>
+    <p>
+      I planeswalker sono alleati potenti a cui puoi fare appello affinché
+      combattano al tuo fianco. Per scoprire maggiori informazioni sui
+      planeswalker, visita la pagina sulle
+      <a href="https://magic.wizards.com/it/game-info/story/planeswalkers/how-planeswalker-cards-work">
+        Regole delle carte Planeswalker
+      </a>
+    </p>
+  </li>
+</ul>
+<h1>Giocare una carta</h1>
+<p>
+  Tutte le carte tranne le terre presentano un costo di mana nell’angolo in alto
+  a destra. Questo costo è composto da simboli che specificano quale tipo di
+  mana devi spendere per lanciare quella magia. Devi pagare il mana rappresentato
+  da ognuno di questi simboli per poter lanciare una carta. Prendiamo un esempio, se su
+  una carta trovo due simboli <img src="Resources/Red_Mana.png" alt="Rosso"> e un
+  simbolo "4" grigio, il simbolo <img src="Resources/Red_Mana.png" alt="Rosso"> significa un mana rosso.
+  Il simbolo "4" grigio significa quattro mana di qualsiasi tipo. Quindi, questo costo
+  deve essere pagato con due mana rossi e quattro mana di qualsiasi tipo.
+</p>
+<p>
+  Per pagare un costo di mana, TAPpa (ruota di lato) le terre necessarie sul campo
+  di battaglia; in questo modo, mostrerai che le hai utilizzate per produrre
+  mana. Le carte TAPpate vengono STAPpate all’inizio di ogni tuo turno, pronte
+  per essere usate di nuovo. Per lanciare il Drago di Shivan, puoi TAPpare due
+  Montagne, che producono mana rosso, e altre quattro terre, che possono essere
+  Montagne, Foreste o altre terre in qualsiasi combinazione.
+</p>
+<h1>combattimento</h1>
+<!-- -->
---
+++ regole.php
@@ -0,0 +1,10 @@
+<?php
+
+  $pagein = array(
+    'Namepage' => 'Regole'
+  );
+
+  require_once ("Views/header.php");
+  require_once ("Views/regole.php");
+  require_once ("Views/footer.php");
+ ?>

977f7a5bf157e8ec1f871fe709137bac271d34a8
Veronica Barbieri <veronica.barbieri.p@gmail.com>
2019-11-28 11:32:12 +0100

    Update techweb_readydb.sql

--- Database/techweb_readydb.sql
+++ Database/techweb_readydb.sql
@@ -42,7 +42,7 @@ CREATE TABLE `carta` (
   `Descrizione` text COLLATE utf8mb4_unicode_ci,
   `Flavour_text` tinytext COLLATE utf8mb4_unicode_ci,
   `Espansione` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Rarita` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Rarita` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `Artista` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `DEF` tinyint(2) UNSIGNED DEFAULT NULL,
   `ATK` tinyint(2) UNSIGNED DEFAULT NULL,
@@ -54,26 +54,26 @@ CREATE TABLE `carta` (
 --
 
 INSERT INTO `carta` (`Id`, `Nome`, `Colore`, `Costo_verde`, `Costo_rosso`, `Costo_blu`, `Costo_bianco`, `Costo_nero`, `Costo_nocolor`, `Tipo`, `Descrizione`, `Flavour_text`, `Espansione`, `Rarita`, `Artista`, `DEF`, `ATK`, `Img_path`) VALUES
-(1, 'Time Sieve', 'Blu', 0, 0, 1, 0, 1, 0, 'Artefatto', 'Sacrifice five artifacts: Take an extra turn\n  after this one.', '\"Prego di non essere mai considerata vecchia o inutile\" -- Sharuum Egemone', 'Alara Reborn', 'Rara', 'Franz', NULL, NULL, 'Resources/carta1.jpg'),
-(2, 'Forest', 'Verde', NULL, NULL, NULL, NULL, NULL, NULL, 'Terra', NULL, NULL, 'Duel Decks', 'Terra', 'John Avon', NULL, NULL, 'Resources/carta4.jpeg'),
-(3, 'Magmaw', 'Rosso', 0, 2, 0, 0, 0, 3, 'Creatura', 'Sacrifice a nonland permanent:\n  Magmaw deals 1 damage to any target.', '\"The purpose of existence is simple:\n  everything is fuel for the magmaw.\" —Jaji, magmaw worshipper', 'Rise of the Eldrazi', 'Rara', 'Karl Kopinski', 4, 4, 'Resources/carta5.jpeg'),
+(1, 'Time Sieve', 'Blu', 0, 0, 1, 0, 1, 0, 'Artefatto', 'Sacrifice five artifacts: Take an extra turn\n  after this one.', '\"Prego di non essere mai considerata vecchia o inutile\" -- Sharuum Egemone', 'Set Base Nona Edizione', 'Rara', 'Franz', NULL, NULL, 'Resources/carta20.jpg'),
+(2, 'Forest', 'Verde', NULL, NULL, NULL, NULL, NULL, NULL, 'Terra', NULL, NULL, 'Set Base Quarta Edizione', 'Terra', 'John Avon', NULL, NULL, 'Resources/carta4.jpeg'),
+(3, 'Magmaw', 'Rosso', 0, 2, 0, 0, 0, 3, 'Creatura', 'Sacrifice a nonland permanent:\n  Magmaw deals 1 damage to any target.', '\"The purpose of existence is simple:\n  everything is fuel for the magmaw.\" —Jaji, magmaw worshipper', 'Set Base 2018', 'Rara', 'Karl Kopinski', 4, 4, 'Resources/carta5.jpeg'),
 (4, 'Plant Elemental', 'Verde', 1, 0, 0, 0, 0, 1, 'Creatura', 'When Plant\n  Elemental enters the battlefield, sacrifice it unless you sacrifice a Forest.', NULL, 'Portal', 'Non comune', 'Ted Naifeh', 4, 3, 'Resources/carta6.jpeg'),
 (5, 'Barren Moor', 'Nero', NULL, NULL, NULL, NULL, NULL, NULL, 'Terra', 'Barren Moor enters the\n  battlefield tapped.', 'Commander 2018', 'Archenemy', 'Comune', ' Heather Hudson', NULL, NULL, 'Resources/carta7.png'),
 (6, 'Mind Raker', 'Nero', 0, 0, 0, 0, 1, 3, 'Creatura', 'Devoid (This card has\n  no color.) When Mind Raker enters the battlefield, you may put a card an\n  opponent owns from exile into that player\'s graveyard. If you do, each\n  opponent discards a card.', 'It consumes any dreams of victory.', 'Battle for\n  Zendika', 'Comune', 'Lius Lasahido', 3, 3, 'Resources/carta8.png'),
-(7, 'Cabal Patriarch', 'Nero', 0, 0, 0, 0, 3, 3, 'Creatura', '(2+B) Sacrifice a\n  creature: Target creature gets -2/-2 until end of turn. (2+B) Exile a creature card\n  from your graveyard: Target creature gets -2/-2 until end of turn.', NULL, 'Odyssey', 'Rara', 'Mark Zug', 5, 5, 'Resources/carta9.jpeg'),
-(8, 'Oriss, Samite Guardian', 'Blu', 0, 0, 2, 0, 1, 0, ' Creatura', 'Prevent\n  all damage that would be dealt to target creature this turn. Grandeur — Discard\n  another card named Oriss, Samite Guardian: Target player can\'t cast spells\n  this turn, and creatures that player controls can\'t attack this turn.', NULL, 'Future Sight', 'Rara', 'Michael Komarck', 3, 1, 'Resources/carta10.jpeg'),
-(9, 'Island', 'Verde', NULL, NULL, NULL, NULL, NULL, NULL, 'Terra', NULL, NULL, ' Throne of\n  Eldrain', 'Terra', 'John Avon', NULL, NULL, 'Resources/carta11.png'),
-(10, 'Swamp', 'Verde', NULL, NULL, NULL, NULL, NULL, NULL, 'Palude', NULL, NULL, ' Throne of\n  Eldrain', 'Terra', 'Paul Scott Canavan', NULL, NULL, 'Resources/carta12.png'),
-(11, 'Battle Mastery', 'Bianco', 0, 0, 0, 1, 0, 2, 'Incantesimo', 'Enchant creature: Enchanted\n  creature has double strike. (It deals both first-strike and regular combat\n  damage.)', '\"There is elegance in all the Ojutai do—even their killing.\"—Kirada\n  , Silumgar enforcer', 'Battlebond', 'Non comune', 'Viktor Titov', NULL, NULL, 'Resources/carta13.png'),
-(12, 'Cobra Trap', 'Verde', 2, 0, 0, 0, 0, 4, ' Istantanea', 'If a noncreature permanent under\n  your control was destroyed this turn by a spell or ability an opponent\n  controlled, you may pay Green rather than pay this spell\'s mana cost. Create\n  four 1/1 green Snake creature tokens.', NULL, ' Magic: The Gatherin', 'Non comune', 'Scott Chou', NULL, NULL, 'Resources/carta14.jpeg'),
-(13, 'Aerial Caravan', 'Bianco', 0, 0, 2, 0, 0, 4, 'Creatura', 'Flying (1+2B): Exile\n  the top card of your library. Until end of turn, you may play that card.\n  (Reveal the card as you exile it.)', 'Successful delivery is not guaranteed.', 'Mercadian Masques', 'Rara', 'DiTerlizzi', 3, 4, 'Resources/carta15.jpeg'),
-(14, 'Act of Aggression', 'Rosso', 0, 2, 0, 0, 0, 2, 'Istantanea', 'Gain control of target\n  creature an opponent controls until the end of turn. Untap that creature. It gains\n  haste until the end of the turn.', NULL, 'Commander 2015', 'Uncommon', 'Whit Brachna', NULL, NULL, 'Resources/carta16.jpg'),
-(15, 'Abbey Griffin', 'Bianco', 0, 0, 0, 1, 0, 3, 'Creatura', 'Flying, vigilance', 'The darkness swarms with vampires and ghouls, but allies are not lacking. —\n   Mikaeus, the Lunarca', 'Innistrad', 'Common', 'Jaime Jones', 2, 2, 'Resources/carta1.jpg'),
-(16, 'Alhammarret, High Arbiter', 'Blu', 0, 0, 2, 0, 0, 5, 'Creatura', 'Flying.\n  As Alhammarret, High Arbiter enters the battlefield, each opponent reveals their hand. You choose\n  the name of a nonland card revealed this way. Your opponents can \'t cast spells with the chosen name\n  (as long as this creature is on the battlefield).)', NULL, 'Magic Origins', 'Rare', 'Richard Wright', 5, 5, 'Resources/carta2.jpg'),
-(17, 'Anowon, the Ruin Sage', 'Nero', 0, 0, 0, 0, 2, 3, 'Creatura', 'At the beginning of your upkeep, each player sacrifices a non-Vampire creature.', '\"Too many have\n  died looking for that map. And now here it is in the hands of the arrogant little girl Chandra Nalaar.\"', 'Commander 2017', 'Rare', 'Dan Scott', 4, 3, 'Resources/carta3.jpg'),
-(18, 'Abundant Growth', 'Verde', 1, 0, 0, 0, 0, 0, 'Incantesimo', 'Enchant land\n  When Abundant Growth enters the battlefield, draw a card. Enchanted land has\n  Tap: Add one mana of any color.', NULL, 'Avacyn Restored', 'Common', 'Vincent Proce', NULL, NULL, 'Resources/carta17.jpg'),
-(19, 'Acid Web Spider', 'Verde', 2, 0, 0, 0, 0, 3, 'Creatura', 'Reach. When Acid Web Spider enters\n  the battlefield, you may destroy target Equipment.', 'His canvas is an excellent source of molten\n  copper for the brave or the foolish.', 'Scars of Mirrodin', 'Uncommon', 'Austin Hsu', 3, 5, 'Resources/carta18.jpg'),
-(20, 'Academy Drake', 'Blu', 0, 0, 1, 0, 0, 2, 'Creatura', 'Kicker 4 (You may pay an additional 4 as you\n  cast this spell). Flying. If Academy Drake was kicked, it enters the battlefield with two +1/+1 counters on it.', NULL, 'Dominaria', 'Common', 'Svetlin Velinov', 2, 2, 'Resources/carta19.jpg');
+(7, 'Cabal Patriarch', 'Nero', 0, 0, 0, 0, 3, 3, 'Creatura', '(2+B) Sacrifice a\n  creature: Target creature gets -2/-2 until end of turn. (2+B) Exile a creature card\n  from your graveyard: Target creature gets -2/-2 until end of turn.', NULL, 'Set Base 2010', 'Rara', 'Mark Zug', 5, 5, 'Resources/carta9.jpeg'),
+(8, 'Oriss, Samite Guardian', 'Bianco', 0, 0, 2, 0, 1, 0, ' Creatura', 'Prevent\n  all damage that would be dealt to target creature this turn. Grandeur — Discard\n  another card named Oriss, Samite Guardian: Target player can\'t cast spells\n  this turn, and creatures that player controls can\'t attack this turn.', NULL, 'Set Base Sesta Edizione', 'Rara', 'Michael Komarck', 3, 1, 'Resources/carta10.jpeg'),
+(9, 'Island', 'Blu', NULL, NULL, NULL, NULL, NULL, NULL, 'Terra', NULL, NULL, ' Throne of\n  Eldrain', 'Terra', 'John Avon', NULL, NULL, 'Resources/carta11.png'),
+(10, 'Swamp', 'Nero', NULL, NULL, NULL, NULL, NULL, NULL, 'Palude', NULL, NULL, ' Throne of\n  Eldrain', 'Terra', 'Paul Scott Canavan', NULL, NULL, 'Resources/carta12.png'),
+(11, 'Battle Mastery', 'Bianco', 0, 0, 0, 1, 0, 2, 'Incantesimo', 'Enchant creature: Enchanted\n  creature has double strike. (It deals both first-strike and regular combat\n  damage.)', '\"There is elegance in all the Ojutai do—even their killing.\"—Kirada\n  , Silumgar enforcer', 'Set Base 2015', 'Non comune', 'Viktor Titov', NULL, NULL, 'Resources/carta13.png'),
+(12, 'Cobra Trap', 'Verde', 2, 0, 0, 0, 0, 4, ' Istantanea', 'If a noncreature permanent under\n  your control was destroyed this turn by a spell or ability an opponent\n  controlled, you may pay Green rather than pay this spell\'s mana cost. Create\n  four 1/1 green Snake creature tokens.', NULL, 'Set Base Quinta Edizione', 'Non comune', 'Scott Chou', NULL, NULL, 'Resources/carta14.jpeg'),
+(13, 'Aerial Caravan', 'Bianco', 0, 0, 2, 0, 0, 4, 'Creatura', 'Flying (1+2B): Exile\n  the top card of your library. Until end of turn, you may play that card.\n  (Reveal the card as you exile it.)', 'Successful delivery is not guaranteed.', 'Set Base 2015', 'Rara', 'DiTerlizzi', 3, 4, 'Resources/carta15.jpeg'),
+(14, 'Act of Aggression', 'Rosso', 0, 2, 0, 0, 0, 2, 'Istantanea', 'Gain control of target\n  creature an opponent controls until the end of turn. Untap that creature. It gains\n  haste until the end of the turn.', NULL, 'Set Base 2015', 'Uncommon', 'Whit Brachna', NULL, NULL, 'Resources/carta16.jpg'),
+(15, 'Abbey Griffin', 'Bianco', 0, 0, 0, 1, 0, 3, 'Creatura', 'Flying, vigilance', 'The darkness swarms with vampires and ghouls, but allies are not lacking. —\n   Mikaeus, the Lunarca', 'Set Base 2019', 'Common', 'Jaime Jones', 2, 2, 'Resources/carta1.jpg'),
+(16, 'Alhammarret, High Arbiter', 'Blu', 0, 0, 2, 0, 0, 5, 'Creatura', 'Flying.\n  As Alhammarret, High Arbiter enters the battlefield, each opponent reveals their hand. You choose\n  the name of a nonland card revealed this way. Your opponents can \'t cast spells with the chosen name\n  (as long as this creature is on the battlefield).)', NULL, 'Set Base Quarta Edizione', 'Rare', 'Richard Wright', 5, 5, 'Resources/carta2.jpg'),
+(17, 'Anowon, the Ruin Sage', 'Nero', 0, 0, 0, 0, 2, 3, 'Creatura', 'At the beginning of your upkeep, each player sacrifices a non-Vampire creature.', '\"Too many have\n  died looking for that map. And now here it is in the hands of the arrogant little girl Chandra Nalaar.\"', 'Set Base Settima Edizione', 'Rare', 'Dan Scott', 4, 3, 'Resources/carta3.jpg'),
+(18, 'Abundant Growth', 'Verde', 1, 0, 0, 0, 0, 0, 'Incantesimo', 'Enchant land\n  When Abundant Growth enters the battlefield, draw a card. Enchanted land has\n  Tap: Add one mana of any color.', NULL, 'Set Base Settima Edizione', 'Common', 'Vincent Proce', NULL, NULL, 'Resources/carta17.jpg'),
+(19, 'Acid Web Spider', 'Verde', 2, 0, 0, 0, 0, 3, 'Creatura', 'Reach. When Acid Web Spider enters\n  the battlefield, you may destroy target Equipment.', 'His canvas is an excellent source of molten\n  copper for the brave or the foolish.', 'Set Base Quinta Edizione', 'Uncommon', 'Austin Hsu', 3, 5, 'Resources/carta18.jpg'),
+(20, 'Academy Drake', 'Blu', 0, 0, 1, 0, 0, 2, 'Creatura', 'Kicker 4 (You may pay an additional 4 as you\n  cast this spell). Flying. If Academy Drake was kicked, it enters the battlefield with two +1/+1 counters on it.', NULL, 'Set Base Ottava Edizione', 'Common', 'Svetlin Velinov', 2, 2, 'Resources/carta19.jpg');
 
 -- --------------------------------------------------------
 
@@ -228,7 +228,7 @@ ALTER TABLE `mazzo`
 -- Indici per le tabelle `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`Username`,`Email`);
+  ADD PRIMARY KEY (`Username`);
 
 --
 -- AUTO_INCREMENT per le tabelle scaricate

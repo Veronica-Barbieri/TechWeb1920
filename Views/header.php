877989e6dcc41408ad6b354a48ab49fcbdbc7475
MCarbona <45630302+MCarbona@users.noreply.github.com>
2020-01-06 18:36:52 +0100

    Update header.php

--- Views/header.php
+++ Views/header.php
@@ -49,19 +49,13 @@
   <a href="#" class="fa fa-instagram"></a>-->
   </nav>
     <div class="header" id="header">
+      <h1 id="title">Magic The Gathering</h1>
+    </div>
     <div class="nb-col1">
       <a class="nb-logo" href="index.php">
         <i class="fab fa-wizards-of-the-coast" style="font-size: 5em;"></i>
       </a>
       <!-- Rivedere gli href per i nomi delle pagine -->
-      <div class="title">Magic The Gathering</div>
-      <ul class="navbar">
-        <li class="nb-item"> <a class="nb-link" href="index.php" xml:lang="en">Home</a> </li>
-        <li class="nb-item"> <a class="nb-link" href="regole.php">Regole</a> </li>
-        <li class="nb-item"> <a class="nb-link" href="cardwiki.php">Wiki Carte</a> </li>
-        <li class="nb-item"> <a class="nb-link" href="deck_forum.php">Forum</a> </li>
-        <li class="nb-item"> <a class="nb-link" href="contacts.php" xml:lang="en">About us</a> </li>
-      </ul>
     </div>
     <?php if(isset($_SESSION["Username"])) { ?>
       <a class='nb-login' href='usr_page.php'>

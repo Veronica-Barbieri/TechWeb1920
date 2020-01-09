1aab55fc200dd2017951417b9c3bc1c6b9a75eaa
MCarbona <45630302+MCarbona@users.noreply.github.com>
2020-01-09 15:27:33 +0100

    Update header.php

--- Views/header.php
+++ Views/header.php
@@ -64,7 +64,7 @@
       </a>
     <?php } else {  ?>
       <a class='nb-login' href='login.php'></a>
-      <i class="fa fa-user"></i>
+      <i href="login.php"class="fa fa-user"></i>
   <?php } ?>
   </div>
 </body>
